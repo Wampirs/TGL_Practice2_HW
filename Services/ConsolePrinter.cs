6c7b915b538dee07bb11b5419b24aac6de0ea02e
Wampirs <79167501+Wampirs@users.noreply.github.com>
2022-07-16 17:11:31 +0300

    Created base services

---
+++ Services/ConsolePrinter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGL_Practice2_HW.Services
+{
+    internal class ConsolePrinter
+    {
+    }
+}
---
+++ Services/Interfaces/IPrinter.cs
@@ -0,0 +1,8 @@
+﻿namespace TGL_Practice2_HW.Services.Interfaces
+{
+    internal interface IPrinter
+    {
+        public void Print(string _ps);
+
+    }
+}
